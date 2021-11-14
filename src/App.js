210d373da3308865398ae8fb0424fc3fc571644b
MattBlanco <matthew.e.blanco@gmail.com>
2021-11-14 01:43:12 -0600

    Added loader formatting

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ const { Handle } = Slider;
 
 function Loader() {
   return (
-    <>
-      <p>Now loading</p>
-      <Spinner animation="border" variant="primary" />
-      <Spinner animation="border" variant="secondary" />
-      <Spinner animation="border" variant="success" />
-      <Spinner animation="border" variant="danger" />
-      <Spinner animation="border" variant="warning" />
-      <Spinner animation="border" variant="info" />
-      <Spinner animation="border" variant="light" />
-      <Spinner animation="border" variant="dark" />
-      <Spinner animation="grow" variant="primary" />
-      <Spinner animation="grow" variant="secondary" />
-      <Spinner animation="grow" variant="success" />
-      <Spinner animation="grow" variant="danger" />
-      <Spinner animation="grow" variant="warning" />
-      <Spinner animation="grow" variant="info" />
-      <Spinner animation="grow" variant="light" />
-      <Spinner animation="grow" variant="dark" />
-    </>
+    <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <h2>Now loading</h2>
+      <div>
+        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="secondary" />
+        <Spinner animation="border" variant="success" />
+        <Spinner animation="border" variant="danger" />
+        <Spinner animation="border" variant="warning" />
+        <Spinner animation="border" variant="info" />
+        <Spinner animation="border" variant="light" />
+        <Spinner animation="border" variant="dark" />
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+        <Spinner animation="grow" variant="danger" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="info" />
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="grow" variant="dark" />
+      </div>
+    </Container>
   );
 }
 
